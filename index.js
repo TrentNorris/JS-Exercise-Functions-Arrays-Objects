38e79f5e9b4b32075434f7eb694c7b1cdcf6c1c9
Trent Norris <trent.norris018@gmail.com>
2019-12-03 03:51:15 -0800

    completed arrow challenges

--- index.js
+++ index.js
@@ -267,8 +267,11 @@ function getOlderCars(inventory, maxYear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  for(let i = 0; i < inventory.length - 1; i++) {
+     
+  }
+  
 }
 
 /**
@@ -289,9 +292,15 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => {
+  return a + b;
+}
+const addFive = (num) => {
+  return num + 5;
+}
+const argTimesTwo = (num) => {
+  return num * 2;
+}
 
 /**
  * ### Challenge `carMaker`
