c5fee959bd29787ad227da3e0e80f62eef57fc2d
Trent Norris <trent.norris018@gmail.com>
2019-12-03 00:43:39 -0800

    person obj created, function updated

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+
+const person = {}
+
+function makePersonObject(id, name, email) {
+  Object.create(person);
+  person.id = id;
+  person.name = name;
+  person.email = email;
 }
 
 /**
